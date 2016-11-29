2069cd6dfbf4c99695ce96bb4d21c2f8567f18b5
Francesco Frosini <ott8bre@gmail.com>
2016-11-29 13:45:42 +0100

    tests updated

---
+++ src/test/java/com/ott8bre/MaybeTest.java
@@ -0,0 +1,46 @@
+package com.ott8bre;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Francesco Frosini <ott8bre@gmail.com>
+ */
+public class MaybeTest {
+
+    /**
+     * Test of withDefault method, of class Maybe.
+     */
+    @Test
+    public void testWithDefault() {
+        System.out.println("withDefault");
+        assertEquals(0, Maybe.nothing().withDefault(0));
+        assertEquals(1, 1*Maybe.just(1).withDefault(0));
+    }
+
+    /**
+     * Test of isJust method, of class Maybe.
+     */
+    @Test
+    public void testIsJust() {
+        System.out.println("isJust");
+        assertFalse(Maybe.nothing().isJust());
+        assertTrue(Maybe.just(1).isJust());
+    }
+
+    /**
+     * Test of isNothing method, of class Maybe.
+     */
+    @Test
+    public void testIsNothing() {
+        System.out.println("isNothing");
+        assertTrue(Maybe.nothing().isNothing());
+        assertFalse(Maybe.just(1).isNothing());
+    }
+    
+}
